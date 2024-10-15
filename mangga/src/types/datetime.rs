a3f13fdb9f7b4aba653a09b00e3eb0c88f3fe16d
Ade M Ramdani <qcynaut@gmail.com>
2024-10-15 15:41:32 +0700

    Switch DateTime to use bson::DateTime and update related implementations.

--- mangga/src/types/datetime.rs
+++ mangga/src/types/datetime.rs
@@ -3,53 +3,19 @@ use serde::{Deserialize, Serialize};
 use std::ops::Deref;
 
 /// DateTime type
-#[derive(Debug, Clone, Default, PartialEq, Eq, PartialOrd, Ord)]
-pub struct DateTime(chrono::DateTime<Utc>);
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
+pub struct DateTime(bson::DateTime);
+
+impl Default for DateTime {
+    fn default() -> Self {
+        Self::now()
+    }
+}
 
 impl DateTime {
     /// Get now
     pub fn now() -> Self {
-        Self(Utc::now())
-    }
-}
-
-impl From<chrono::DateTime<Utc>> for DateTime {
-    fn from(dt: chrono::DateTime<Utc>) -> Self {
-        Self(dt)
-    }
-}
-
-impl Deref for DateTime {
-    type Target = chrono::DateTime<Utc>;
-
-    fn deref(&self) -> &Self::Target {
-        &self.0
-    }
-}
-
-impl AsRef<chrono::DateTime<Utc>> for DateTime {
-    fn as_ref(&self) -> &chrono::DateTime<Utc> {
-        &self.0
-    }
-}
-
-impl Serialize for DateTime {
-    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-    where
-        S: serde::Serializer,
-    {
-        serializer.serialize_str(&self.0.to_rfc3339())
-    }
-}
-
-impl<'de> Deserialize<'de> for DateTime {
-    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
-    where
-        D: serde::Deserializer<'de>,
-    {
-        let s = String::deserialize(deserializer)?;
-        let dt = chrono::DateTime::parse_from_rfc3339(&s).map_err(serde::de::Error::custom)?;
-        Ok(Self(dt.to_utc()))
+        Self(bson::DateTime::from(Utc::now()))
     }
 }
 
@@ -58,7 +24,7 @@ impl async_graphql::ScalarType for DateTime {
     fn parse(value: async_graphql::Value) -> async_graphql::InputValueResult<Self> {
         if let async_graphql::Value::String(s) = &value {
             if let Ok(dt) = chrono::DateTime::parse_from_rfc3339(&s) {
-                return Ok(Self(dt.to_utc()));
+                return Ok(Self(bson::DateTime::from(dt)));
             }
         }
 
@@ -66,6 +32,6 @@ impl async_graphql::ScalarType for DateTime {
     }
 
     fn to_value(&self) -> async_graphql::Value {
-        async_graphql::Value::String(self.0.to_rfc3339())
+        async_graphql::Value::String(chrono::DateTime::from(self.0).to_rfc3339())
     }
 }
